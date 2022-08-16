b6e8f7a2bd81ac96a6b02b42ebd21c6832a1cc67
egor594bed <egor594work@gmail.com>
2022-08-16 23:51:18 +0300

    0.2

--- Mirerals/assets/js/main-dist.js
+++ Mirerals/assets/js/main-dist.js
@@ -19,9 +19,12 @@
     
 // }, 500)
 
-const intro = document.querySelector(".intro");
-const introAbout = document.querySelector(".intro-about");
-const introAboutDecor = document.querySelector(".intro-about__decor");
+const intro = document.querySelector(".intro"),
+      introAbout = document.querySelector(".intro-about"),
+      introAboutDecor = document.querySelector(".intro-about__decor"),
+      mainNewsArrowPrev = '<div class="nav__arrow nav__arrow--prev"><svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M13 6L8 11L3 6" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+      mainNewsArrowNext = '<div class="nav__arrow nav__arrow--next"><svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M13 6L8 11L3 6" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+
 
 document.addEventListener("scroll", function (e) {
     if ((introAbout.offsetTop * 0.2) < window.pageYOffset) {
@@ -29,4 +32,37 @@ document.addEventListener("scroll", function (e) {
     }
 });
 
+$(document).ready(function(){
+    $('.service-slider__wrapper').slick({
+        horizontal: true,
+        horizontalSwiping: true,
+        touchThreshold: true,
+        dots: false,
+        arrows: false,
+        infinite: false, 
+        slidesToShow: 4,
 
+    });
+
+    $('.service-slider__wrapper').on('wheel', (function(e) {
+    e.preventDefault();
+    if (e.originalEvent.deltaY > 0) {
+        $(this).slick('slickNext');
+    } else {
+        $(this).slick('slickPrev');
+    }}));
+
+
+
+    $(".main-news-slider").slick ({
+        dots: false,
+        appendArrows: $(".main-news__top-nav"),
+        prevArrow: mainNewsArrowPrev,
+        nextArrow: mainNewsArrowNext,
+        horizontal: true,
+        horizontalSwiping: true,
+        infinite: false,
+        slidesToShow: 5, 
+
+    })
+});
